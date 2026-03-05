b16cdf07a894e8330add2daf6e7074b3e2a3f5b0
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-03-05 09:35:31 -0600

    setup: configure Vite + React project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import AppRoutes from './router';
+
+export default function App() {
+    return (
+        <>
+            <AppRoutes />
+        </>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import ScrollToTop from '@shared/utils/ScrollToTop';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <ScrollToTop />
+        <App />
+    </BrowserRouter>,
+);
