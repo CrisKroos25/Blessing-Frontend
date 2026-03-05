7d37c24b2beef5fcaae417f9023079300ea7797e
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-03-05 10:47:27 -0600

    feature(inventory): add purchases page

---
+++ src/features/purchases/pages/PurchasesPage.jsx
@@ -0,0 +1,11 @@
+const PurchasesPage = () => {
+    return (
+        <div>
+            <h2>
+                Esto es la pagina de compras.... 👷
+            </h2>
+        </div>
+    )
+};
+
+export default PurchasesPage;
