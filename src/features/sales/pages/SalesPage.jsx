4c05771dbd211ffcd385fc81d4bc81da382b22af
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-03-05 10:48:28 -0600

    feature(inventory): add sales pagea

---
+++ src/features/sales/pages/SalesPage.jsx
@@ -0,0 +1,10 @@
+export default function SalesPage() {
+
+    return (
+        <div>
+            <h2>
+                Esto es la pagina de ventas.... 👷
+            </h2>
+        </div>
+    )
+}
