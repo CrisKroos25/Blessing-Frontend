ba08c1bae503b9fec5c8b35ae98c90a0cfe0a682
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-03-09 18:02:26 -0600

    refactor(HeaderTitleTable): The header was adjusted to receive parameters.

--- src/shared/components/titleTable/HeadTitleTable.jsx
+++ src/shared/components/titleTable/HeadTitleTable.jsx
@@ -1,22 +1,16 @@
-import styles from "./HeadTitleTable.module.css";
+import styles from './HeadTitleTable.module.css';
 
 export default function SectionHeader({ title, subtitle, action }) {
     return (
         <div className={styles.container}>
             <div className={styles.textContainer}>
-
                 <h2 className={styles.title}>{title}</h2>
                 {subtitle && (
-                <span className={styles.subtitle}>{subtitle}</span>
+                    <span className={styles.subtitle}>{subtitle}</span>
                 )}
-
             </div>
 
-            {action && (
-                <div>   
-                {action}
-                </div>
-            )}
+            {action && <div>{action}</div>}
         </div>
     );
 }
