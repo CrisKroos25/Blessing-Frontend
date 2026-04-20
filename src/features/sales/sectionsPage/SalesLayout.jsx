13c4c13f457c5ce56643aafa2e6353948c82fa6a
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-04-20 12:40:15 -0600

    feature(sales): The main layout for sales was created.

---
+++ src/features/sales/sectionsPage/SalesLayout.jsx
@@ -0,0 +1,29 @@
+// Componente contenedor del contenido principal del ventas.
+
+import styles from './SalesLayout.module.css';
+import SalesIncomeView from '../views/SalesIncomeView';
+import SalesRecordView from '../views/SalesRecordView';
+import SalesTabs from '../components/tabs/SalesTabs';
+import { useState } from 'react';
+
+// se renderiza dinámicamente el componente
+const VIEW_COMPONENTS = {
+    salesIncomeView: SalesIncomeView,
+    salesRecordView: SalesRecordView,
+};
+
+export default function SalesLayout({}) {
+    const [view, setView] = useState('salesIncomeView'); // `view` guarda la key del tab activo.
+
+    const CurrentView = VIEW_COMPONENTS[view]; // Obtenemos el componente que corresponde al tab activo
+
+    return (
+        <div className={styles.container__main}>
+            {/* InventoryTabs recibe el tab activo y la función para cambiarlo cuando el usuario hace click */}
+            <SalesTabs view={view} setView={setView} />
+
+            {/* CurrentView && evita un error si la key no existe en VIEW_COMPONENTS — renderiza solo si es válido */}
+            {CurrentView && <CurrentView />}
+        </div>
+    );
+}
