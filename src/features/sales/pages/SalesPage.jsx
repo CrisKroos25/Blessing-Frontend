a01516ac5882e73ae7cd27299c807aee5eda8c1c
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2026-05-06 01:42:28 -0600

    refactor(sales): The component from which useSales is called was changed.

--- src/features/sales/pages/SalesPage.jsx
+++ src/features/sales/pages/SalesPage.jsx
@@ -1,9 +1,18 @@
 import SalesLayout from '../sectionsPage/SalesLayout';
+import { useSales } from '../hooks/useSales';
 
 export default function SalesPage() {
+    const { products, sales, createSale, isLoading, error } = useSales();
+
     return (
         <>
-            <SalesLayout />
+            <SalesLayout
+                products={products}
+                sales={sales}
+                createSale={createSale}
+                loading={isLoading}
+                error={error}
+            />
         </>
     );
 }
